e69d1bda2ffbd102099da881e4629ec49fac550c
DarshanPH <darshanhegde5@gmail.com>
2021-08-30 19:48:20 +0530

    second commit

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() text?: string;
+  @Input() color?: string;
+  @Output() btnclick = new EventEmitter();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(){
+    this.btnclick.emit();
   }
 
 }
---
+++ src/app/moack-task.ts
@@ -0,0 +1,23 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: 'doctor appointmenet',
+        day: 'may 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'school meeting',
+        day: 'May 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'food shopping',
+        day: 'May 7th at 12:30pm',
+        reminder: true,
+    },
+    
+]
